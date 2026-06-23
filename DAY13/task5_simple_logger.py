6541ea96a7a40a8b420620eaf1f1714d11dfb33f
Nasim irfanbhai <nasimirfanbhaivahora@gamil.come>
2026-06-23 17:45:46 +0530

    paython intanship is create

---
+++ DAY13/task5_simple_logger.py
@@ -0,0 +1,19 @@
+import datetime
+
+# Bonus: Add timestamp to make it realistic
+time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# First action
+with open("log.txt", "a") as file:
+    file.write(f"[{time_now}] User logged in\n")
+
+with open("log.txt", "a") as file:
+    file.write(f"[{time_now}] User performed action\n")
+
+print("Logs written. Current logs:\n")
+
+try:
+    with open("log.txt", "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    pass
